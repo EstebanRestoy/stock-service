f5681d3bf8eee6d893992628175a7735a075a252
EstebanRestoy <esteban.restoy@etu.uca.fr>
2021-05-20 22:45:03 +0200

    [PATCH] error db

--- src/main/java/com/stock/stock/repository/IStockRepository.java
+++ src/main/java/com/stock/stock/repository/IStockRepository.java
@@ -1,8 +1,8 @@
 package com.stock.stock.repository;
 
 import com.stock.stock.entity.Book;
-        import org.springframework.data.repository.CrudRepository;
-        import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IStockRepository extends CrudRepository<Book, String> {
